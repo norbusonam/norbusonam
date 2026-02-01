2aff1eb771c32a3b379ccb807d7308c0202fe6db
Norbu Sonam <nsonam1822@gmail.com>
2026-01-31 22:34:17 -0800

    sherpa / formatting

--- simple/js/script.js
+++ simple/js/script.js
@@ -1,6 +1,6 @@
 const appHeight = () => {
-  const doc = document.documentElement
-  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
- }
- window.addEventListener('resize', appHeight)
- appHeight()
+  const doc = document.documentElement;
+  doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+};
+window.addEventListener("resize", appHeight);
+appHeight();
